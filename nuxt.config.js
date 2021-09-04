20f4343186b568e8cd14b217a0f381f76f553446
Julian Restrepo <julianrestrepo@seven4n.com>
2021-09-04 11:53:35 -0500

    commit inicial

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,5 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "Bienes Raices",
     htmlAttrs: {
@@ -16,25 +13,10 @@ export default {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    "@nuxtjs/tailwindcss"
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
+  buildModules: ["@nuxtjs/tailwindcss"],
   modules: [],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {}
 };
